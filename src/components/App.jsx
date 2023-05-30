71a76e8a9e7aa6eb69329cbaaa390f4fcc3e7242
NoBrainer <vinniemac189@gmail.com>
2023-05-29 21:39:24 -0400

    Created the base of a React app

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>5e-books</h1>
+      </header>
+      <main>
+        <Typography>[UNDER CONSTRUCTION]</Typography>
+      </main>
+    </>
+  );
+}
+
+export default App;
